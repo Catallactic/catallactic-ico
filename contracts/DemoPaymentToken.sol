993ade04345c7e429ede2be40079daa1d3b6d4c9
pellyadolfo <pellyadolfo@yahoo.es>
2023-03-29 23:04:20 +0200

    added comments for test tokens

--- contracts/DemoPaymentToken.sol
+++ contracts/DemoPaymentToken.sol
@@ -8,6 +8,11 @@
  *	╚██████╔╝██║░░██║██████╔╝╚█████╔╝███████╗██║╚█████╔╝██║░╚██╗
  *	░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚══════╝╚═╝░╚════╝░╚═╝░░╚═╝
  *
+ * This token is just a plain sample ERC-20 used to test the crowdsale contracts. It represents a generic payemnt token for testnets.
+ *
+ * *********************************************************************************
+ * THIS TOKEN IS NOT TO BE AUDITED. IS ONLY INCLUDED FOR TEST PURPOSES.
+ * *********************************************************************************
  * 
  */
 // SPDX-License-Identifier: MIT
